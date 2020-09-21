6498f5ccec217e9d15682b6f991e1c686496bcab
gnebbia <16106651+gnebbia@users.noreply.github.com>
2020-09-21 23:26:34 +0200

    Fixed travis config

--- .travis/build_binaries.sh
+++ .travis/build_binaries.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+
+RELEASE_VER=$(tr -d "v." <<<"$TRAVIS_TAG")
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-  pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${TRAVIS_TAG}_osx
+  pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${RELEASE_VER}_osx
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then
-  pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${TRAVIS_TAG}_win
+  pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${RELEASE_VER}_win
 else
-  pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${TRAVIS_TAG}_linux
+  pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${RELEASE_VER}_linux
 fi
 

0daad4d5c566d56ab2f5be33c7337c1110295929
gnebbia <16106651+gnebbia@users.noreply.github.com>
2020-09-21 23:49:37 +0200

    Tuned Travis-CI configuration

--- .travis/build_binaries.sh
+++ .travis/build_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-RELEASE_VER=$(tr -d "v." <<<"$TRAVIS_TAG")
+V_REMOVED=$(tr -d "v" <<< "$TRAVIS_TAG")
+RELEASE_VER=$(tr "." "_" <<< "$V_REMOVED")
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
   pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${RELEASE_VER}_osx
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then

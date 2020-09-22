c6e4712ffbc1184f4de35b1fbccf8b1c3aa99b9e
gnebbia <16106651+gnebbia@users.noreply.github.com>
2020-09-22 13:53:34 +0200

    Merged Travis-ci config from PR #24

--- .travis/build_binaries.sh
+++ .travis/build_binaries.sh
@@ -2,6 +2,7 @@
 
 V_REMOVED=$(tr -d "v" <<< "$TRAVIS_TAG")
 RELEASE_VER=$(tr "." "_" <<< "$V_REMOVED")
+
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
   pyinstaller --paths kb/ --onefile kb/__main__.py -n kb_${RELEASE_VER}_osx
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then
--- .travis/install.sh
+++ .travis/install.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-if [ $TRAVIS_OS_NAME = 'osx' ]; then
-    pip3 install --upgrade pip
-    pip3 install --upgrade setuptools
-    pip3 install -r requirements-dev.txt
-    pip3 install .
-    pip3 install --upgrade pyinstaller
-elif [ $TRAVIS_OS_NAME = 'windows' ]; then
-    python -m pip install --upgrade pip
-    python -m pip install --upgrade setuptools
-    python -m pip install -r requirements-dev.txt
-    python -m pip install .
-    python -m pip install --upgrade pyinstaller
+if [ $TRAVIS_OS_NAME = 'windows' ]; then
+    PYTHON="py"
 else
-    pip install --upgrade pip
-    pip install --upgrade setuptools
-    pip install -r requirements-dev.txt
-    pip install .
-    pip install --upgrade pyinstaller
+    PYTHON="python3"
 fi
 
+$PYTHON -m pip install --upgrade pip setuptools
+$PYTHON -m pip install -r requirements-dev.txt
+$PYTHON -m pip install .
